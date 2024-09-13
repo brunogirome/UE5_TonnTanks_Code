865efd09d24ba61c560f67ab472cc9506b60f83e
Bruno Girome <brunogirome@gmail.com>
2024-09-13 18:01:17 -0300

    created on hit event and debugged some information on hit

--- Projectile.cpp
+++ Projectile.cpp
@@ -17,9 +17,28 @@ AProjectile::AProjectile()
 void AProjectile::BeginPlay()
 {
 	Super::BeginPlay();
+
+	if (BaseMesh)
+	{
+		BaseMesh->OnComponentHit.AddDynamic(this, &AProjectile::OnHit);
+	}
 }
 
 void AProjectile::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+}
+
+void AProjectile::OnHit(
+		UPrimitiveComponent *HitComp,
+		AActor *OtherActor,
+		UPrimitiveComponent *OtherComp,
+		FVector NormalImpulse,
+		const FHitResult &Hit)
+{
+	UE_LOG(LogTemp, Display, TEXT("The dood shooting: %s - HitComp: %s, OtherActor: %s, OtherComp: %s"),
+				 *GetActorNameOrLabel(),
+				 *HitComp->GetName(),
+				 *OtherActor->GetName(),
+				 *OtherComp->GetName());
 }
--- Projectile.h
+++ Projectile.h
@@ -5,6 +5,7 @@
 #include "Projectile.generated.h"
 
 class UProjectileMovementComponent;
+class UPrimitiveComponent;
 
 UCLASS()
 class TOONTANKS_API AProjectile : public AActor
@@ -25,11 +26,19 @@ private:
 	UStaticMeshComponent *BaseMesh;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
-	UProjectileMovementComponent* ProjectileMovementComponent;
+	UProjectileMovementComponent *ProjectileMovementComponent;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile", meta = (AllowPrivateAccess = "true"))
 	float InitialSpeed = 40.f;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile", meta = (AllowPrivateAccess = "true"))
 	float MaxSpeed = 60.f;
+
+	UFUNCTION()
+	void OnHit(
+			UPrimitiveComponent *HitComp,
+			AActor *OtherActor,
+			UPrimitiveComponent *OtherComp,
+			FVector NormalImpulse,
+			const FHitResult &Hit);
 };
