70cbc870b53707621ade925693bba7d3adeb3ab1
Bruno Girome <brunogirome@gmail.com>
2024-09-15 00:11:32 -0300

    Damage delegation and health component creation

---
+++ HealthComponent.cpp
@@ -0,0 +1,27 @@
+#include "HealthComponent.h"
+
+UHealthComponent::UHealthComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UHealthComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	HealthValue = MaxHealth;
+
+	if (AActor *owner = GetOwner())
+	{
+		owner->OnTakeAnyDamage.AddDynamic(this, &UHealthComponent::OnDamageTaken);
+	}
+}
+
+void UHealthComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
+
+void UHealthComponent::OnDamageTaken(AActor *DamagedActor, float Damage, const UDamageType *DamageType, AController *Instigator, AActor *DamageCauser)
+{
+}
---
+++ HealthComponent.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "HealthComponent.generated.h"
+
+class AController;
+
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class TOONTANKS_API UHealthComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UHealthComponent();
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(EditAnywhere)
+	float MaxHealth = 100.f;
+
+	float HealthValue = 0.f;
+
+	UFUNCTION()
+	void OnDamageTaken(AActor *DamagedActor, float Damage, const UDamageType *DamageType, AController *Instigator, AActor *DamageCauser);
+
+public:
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) override;
+};
